8679d4b3d68b124a3ccdb97ed305b6165dfe2b6f
kavita vakkund <kavitavakkund6102001@gmail.com>
2024-05-17 00:04:55 -0700

    added new.c and modified READme.md

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # yocto
 Patches 
 Author : Kavita Vakkund
+"Read me file"
+Latest commit =Last name
---
+++ new.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    int num1=10, int num2=20;
+    int sum=num1+num2;
+    printf("Sum of two numbers is : %d",sum);
+    return 0;
+}
